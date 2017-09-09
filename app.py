dcdd8860ae3be31ac4fc430d2279c78bffe9855a
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-09 20:21:56 +0000

    added ssh

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Welcome to gcp"
+@app.route("/hello")
+def hello():
+    return "Hello "
+
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'), debug=True,threaded=True, use_reloader=True)
