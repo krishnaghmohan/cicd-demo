f1f727093f24734f491b480c6c5709a4f2e79738
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-10 11:03:22 +0000

    new branch

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to gcp iCompute"
+    return "Welcome to gcp World Compute"
 @app.route("/hello")
 def hello():
     return "Hello Mr "
