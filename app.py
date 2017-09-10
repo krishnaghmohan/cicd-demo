36325ae5dee89903adb8499871a3d498a0d2acb5
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-10 11:44:52 +0000

    ed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to gcp World Computex"
+    return "Welcome to gcp World Computexya"
 @app.route("/hello")
 def hello():
     return "Hello Mr "
